35a59fd9e87ce2f17afe2ca5c1e29f64381682f3
remyampr <remyaradhakrishnan2992@gmail.com>
2024-12-18 14:00:03 +0400

    add home page with movie cards

--- movie.js
+++ movie.js
@@ -2,13 +2,89 @@ const inputGroup = document.getElementById("input-group");
 const inputBox = document.getElementById("input-box");
 const searchButton = document.getElementById("search-button");
 const resultContainer = document.getElementById("result-container");
-const errorMsgBox = document.getElementById("error-msg-box");
-errorMsgBox.textContent = "";
+const homeMovieContainer = document.getElementById("home-movie-container");
+const homeMovieContainerRow = document.getElementById(
+  "home-movie-container-row"
+);
 //  api key from https://www.omdbapi.com/
 let apikey = "ffcefe9e";
+// starting from page1 in api
+let currentPage = 1;
+// fro getting all movies contains  "hero"
+const searchMovieKey = "hero";
 
-// get data from https://www.omdbapi.com/ api
-async function getData(movieName) {
+// automatically calls when page load
+getFullMovies();
+
+// whem home clicked
+function displayHome() {
+  homeMovieContainer.style.display = "block";
+  resultContainer.textContent = "";
+  // getFullMovies();
+}
+async function getFullMovies() {
+  //  fetch all movies with action and calldisplayMovieCard with that movies to display on home page
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apikey}&s=${searchMovieKey}&page=${currentPage}`
+    );
+    console.log("Response :", response);
+    const moviesList = await response.json();
+    console.log("moviesList: ", moviesList);
+    console.log(moviesList.Search);
+
+    if (moviesList.Response === "True") {
+      displayMovieCards(moviesList.Search);
+      currentPage++;
+    } else {
+      console.error("Error fetching movies:", data.Error);
+    }
+  } catch (error) {
+    console.log("Error occured : ", error);
+  }
+}
+function displayMovieCards(movies) {
+  // iterate through array and append it to home
+  movies.forEach((element) => {
+    // console.log("MOVIESSS : ",element );
+    // console.log("**************&&&&&&&&&***********");
+    let src = element.Poster;
+    let movieCardCol = document.createElement("div");
+    movieCardCol.classList.add("col-12", "col-sm-6", "col-md-4", "col-lg-3","movie-card-col");
+    movieCardCol.innerHTML = ` <div class="card movie-container-card">
+              <img src=${src} class="card-img-top" alt="${element.Title}poster"
+              />
+              <div class="card-body">
+                <a
+                  href="#"
+                  class="link-info link-underline-info link-underline-opacity-10 link-opacity-50-hover"
+                  id="movie-${element.Title}"
+                  >See more</a
+                >
+              </div>
+            </div>`;
+
+    homeMovieContainerRow.appendChild(movieCardCol);
+
+    // when clicke on see more set an event listner so it can direct to display that movie detail using function  getSearchedMovie(moviename)
+    const linkSeeMore = document.getElementById(`movie-${element.Title}`);
+    linkSeeMore.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      console.log("movie search frome home , Movie name :", element.Title);
+      getSearchedMovie(element.Title);
+    });
+  });
+}
+// to fetch more movies when window end
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    getFullMovies();
+  }
+});
+
+// get data from https://www.omdbapi.com/ api with title(movie name)
+async function getSearchedMovie(movieName) {
   try {
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=${apikey}&t=${movieName}`
@@ -26,26 +102,28 @@ async function getData(movieName) {
 //  when click on search button
 function searchMovie() {
   let movieName = inputBox.value;
-  console.log("Input movieName :", movieName);
+  console.log("Inside searchMovie function: Input movieName :", movieName);
   // if there is no input
   if (!movieName) {
-    errorMsgBox.innerHTML = "<h4>Enter a movie name!</h4>";
-    resultContainer.textContent="";
+    alert("Enter a movie name !");
+    resultContainer.textContent = "";
     console.log("error msg");
   }
-  // if there is data entered on input box with that name call getdata to fetch api
+  // if there is data entered on input box  call getSearchedMovie() to fetch api
   else {
-    errorMsgBox.textContent = "";
-    getData(movieName);
+    homeMovieContainer.style.display = "none";
+    getSearchedMovie(movieName);
   }
 }
 function showMovieDetails(data) {
+  homeMovieContainer.style.display = "none";
   let src = data.Poster;
   console.log(src);
-  console.log("Movie name: ", data.Title);
+  console.log("Inside showMovieDetails function Movie name: ", data.Title);
+  // if the entered data is not found
   if (data.Title === undefined) {
-    resultContainer.innerHTML = " <h1>Sorry !!! No movie found!!!</h1>";
-    inputBox.value="";
+    resultContainer.innerHTML = " <h1>Sorry !!! Movie not found!!!</h1>";
+    inputBox.value = "";
     return;
   }
 
@@ -71,5 +149,5 @@ function showMovieDetails(data) {
           </div>
         </div>
       </div>`;
-      inputBox.value="";
+  inputBox.value = "";
 }
